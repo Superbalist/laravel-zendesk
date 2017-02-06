b530df6796a0ff1d1e51c241a33bcf61ee0cea50
Matthew Goslett <matthew@goslett.co.za>
2017-02-06 06:49:56 +0000

    Apply fixes from StyleCI

--- src/ZendeskFacade.php
+++ src/ZendeskFacade.php
@@ -1,11 +1,11 @@
 <?php
+
 namespace Superbalist\Zendesk;
 
 use Illuminate\Support\Facades\Facade;
 
 class ZendeskFacade extends Facade
 {
-
     /**
      * Get the registered name of the component.
      *
